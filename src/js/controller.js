4626c9fad836b429598ed0709bbd290ce3624b8e
Rakiit <ricardoiarced@gmail.com>
2023-08-14 15:47:16 -0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the application')
+}
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,5 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
