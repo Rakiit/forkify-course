30b572b29ba32c85dfad87d83d0f24286f16a55c
Rakiit <ricardoiarced@gmail.com>
2023-08-14 16:39:16 -0700

    Cleaned up unnecessary comments

--- src/js/controller.js
+++ src/js/controller.js
@@ -98,7 +98,6 @@ const controlAddRecipe = async function (newRecipe) {
 
     //     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
 
     //     // Render recipe
     recipeView.render(model.state.recipe);
@@ -121,9 +120,7 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-const newFeature = function () {
-  console.log('Welcome to the application')
-}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,6 +129,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -38,7 +38,6 @@ export const loadRecipe = async function (id) {
             state.recipe.bookmarked = true;
         else state.recipe.bookmarked = false;
 
-        console.log(state.recipe);
     } catch (err) {
         // Temp error handling
         console.error(`${err} !!!`);
@@ -49,7 +48,6 @@ export const loadSearchResults = async function (query) {
     try {
         state.search.query = query;
         const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
-        console.log(data);
 
         state.search.results = data.data.recipes.map(rec => {
             return {
